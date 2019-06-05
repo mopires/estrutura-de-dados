9fa26ab9bec5becbf5939889678eb26302fed957
Matheus Pires <mopires23@gmail.com>
2019-06-05 11:54:29 -0300

    Add files via upload

---
+++ Nodo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabs_estrutura_isso
+{
+    class Nodo
+    {
+
+        private int Info;
+        private Nodo[] nodo;
+        
+
+        public Nodo(int Info)
+        {
+            this.Info = Info;
+        }
+
+        // chave é a referencia para o nodo anterior 
+        // quando criar um novo nodo precisa ser verificado se a chave é menor que o anterior
+        // se igual escolhe um lado. verificar a forma que são inserido os dados
+
+        public void Mostrar()
+        {
+
+            for (int i = 0; i < nodo.Length; i++)
+            {
+                
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabs_estrutura_isso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nodo Arvore = new Nodo(5);
+            for (int i = 0; i < 5; i++)
+            {
+                Arvore.Add(i);
+            }
+
+            Arvore.Mostrar();
+
+
+
+
+
+
+        }
+    }
+}
